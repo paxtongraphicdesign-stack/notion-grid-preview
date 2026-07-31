a0a1bd2e4da5a30edff70f0f7d5743d1daf488fe
paxtongraphicdesign-stack <paxtongraphicdesign@gmail.com>
2026-07-31 00:33:42 -0400

    Fix pino serializer types

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,13 +1,19 @@
 import cors from "cors";
 import express, { type Express } from "express";
-import {
-  pinoHttp,
-  type SerializedRequest,
-  type SerializedResponse,
-} from "pino-http";
+import { pinoHttp } from "pino-http";
 
 import { logger } from "./lib/logger";
 import router from "./routes";
+
+type LogRequest = {
+  id?: string | number;
+  method?: string;
+  url?: string;
+};
+
+type LogResponse = {
+  statusCode?: number;
+};
 
 const app: Express = express();
 
@@ -15,14 +21,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req: SerializedRequest) {
+      req(req: LogRequest) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res: SerializedResponse) {
+      res(res: LogResponse) {
         return {
           statusCode: res.statusCode,
         };
