c4b0fbbe897608149acc4e87bf00e4bc4d896615
paxtongraphicdesign-stack <paxtongraphicdesign@gmail.com>
2026-07-31 00:30:40 -0400

    Fix API TypeScript build errors

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,13 @@
-import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes";
+import express, { type Express } from "express";
+import {
+  pinoHttp,
+  type SerializedRequest,
+  type SerializedResponse,
+} from "pino-http";
+
 import { logger } from "./lib/logger";
+import router from "./routes";
 
 const app: Express = express();
 
@@ -10,14 +15,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: SerializedRequest) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: SerializedResponse) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +30,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
